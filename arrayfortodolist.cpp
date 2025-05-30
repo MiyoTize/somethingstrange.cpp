4e3a5988ca2b2a9815db67a02fa154e2711caa3c
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-30 23:17:11 +0300

    Update arrayfortodolist.cpp

--- arrayfortodolist.cpp
+++ arrayfortodolist.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 
 int main() {
-	char answer;
-	int array[]{};
-	int oldsize = 0, newsize{};
-	std::cout << "Heyo! Miyo here!"
+  char answer;
+  int array[]{};
+  int oldsize = 0, newsize{};
+  std::cout << "Heyo! Miyo here!"
             << "Do you want write new task?\n(Y/n):";
   std::cin >> answer;
 
-	while(answer == 'y') {
-	  std::cout << "Write new task: ";
-	  newsize = ++oldsize;
-	  std::cin >> array[newsize];
-	  std::cout << newsize << " " << array[newsize] << "\n";
+  while(answer == 'y') {
+    std::cout << "Write new task: ";
+    newsize = ++oldsize;
+    std::cin >> array[newsize];
+    std::cout << newsize << " " << array[newsize] << "\n";
 
-		while( oldsize < newsize ) {
-  	  std::cout << *array << "\n";
-	    std::cout << array[newsize] << "\n";
-		  newsize = sizeof(array) / sizeof(array[0]);
-		  ++oldsize;
+    while( oldsize < newsize ) {
+      std::cout << *array << "\n";
+      std::cout << array[newsize] << "\n";
+      newsize = sizeof(array) / sizeof(array[0]);
+      ++oldsize;
     }
     std::cout << "Do you want write new task?\n(Y/n):";
     std::cin >> answer;
-	}
-
+  }
   std::cout << "Bye!"; 
   return 0;
 }
